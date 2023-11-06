fe04aa4b14db0966a9be8e6e28f47378bbfbec40
MARCOS VINICIUS DA SILVA ROSA <rosa.3950@aluno.pr.senac.br>
2023-11-06 17:29:27 -0300

    'categoria'

---
+++ src/main/java/com/mycompany/ferramentas/DadosTemporarios.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ferramentas;
+
+/**
+ *
+ * @author rosa.3950
+ */
+public class DadosTemporarios {
+    public static Object tempObject;
+    public static Object tempObject2;
+    public static Object tempObject3;
+    
+  
+    public static int idUsuarioLogado;
+    public static String usuarioLogado;
+    
+
+    public static String categoriaProdutoVenda;
+    public static String marcaProdutoVenda;
+}
---
+++ src/main/java/com/mycompany/ferramentas/Formularios.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ferramentas;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author rosa.3950
+ */
+public class Formularios {
+ 
+    public static JFrame cadCategoria;
+    public static JFrame listCategoria;
+    
+}
